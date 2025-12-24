7d1c135ae8da0ff488ee922ff2da7a3127039e4a
AnkanWork1 <ankang.hestabit@gmail.com>
2025-12-24 19:25:46 +0530

    added

---
+++ nextJS/my-next-app/abc/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-4 bg-blue-100 rounded">
+      <h2>Home Component Content</h2>
+      <p>This is content from Home.jsx</p>
+    </div>
+  );
+}
